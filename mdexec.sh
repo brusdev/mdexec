bed9ecde58d883f9ad5b19e555dbcfe7bc5781cc
Domenico Francesco Bruscino <brusdev@apache.org>
2024-05-29 15:50:12 +0200

    Add script mdexec.sh

---
+++ mdexec.sh
@@ -0,0 +1,26 @@
+set -e
+
+MDFILE=$1
+
+while read LINE; do
+    SHELL_SCRIPT="$(dirname ${MDFILE})/~$(basename ${MDFILE}).sh"
+    if echo "${LINE}" | grep --quiet '```bash exec="on"'; then
+        EXPECTED_RETURN_CODE="$(echo "${LINE}" | grep -oP '(?<=returncode=")[^"]+')"
+        PARSER_STATE='BASH_SCRIPT'
+        echo '#!/bin/bash' > ${SHELL_SCRIPT}
+    elif echo "${LINE}" | grep --quiet '```'; then
+        PARSER_STATE=''
+
+        source ${SHELL_SCRIPT}
+        ACTUAL_RETURN_CODE=$?
+
+        if [ "${ACTUAL_RETURN_CODE}" != "${EXPECTED_RETURN_CODE}" ]; then
+            echo "Unexpected return code ${ACTUAL_RETURN_CODE}/${EXPECTED_RETURN_CODE}"
+            exit 1
+        fi
+
+        rm ${SHELL_SCRIPT}
+    else
+        echo "${LINE}" >> ${SHELL_SCRIPT}
+    fi
+done < ${MDFILE}
